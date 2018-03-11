287b79e76e75dd99422d9e64e3585c6bd401d2b7
Jason Held <jasonsheld@gmail.com>
2018-03-11 16:41:50 -0400

    make run_evaluation more easy to script against.

--- run_evaluation.py
+++ run_evaluation.py
@@ -12,6 +12,53 @@ from setup_network import find_per_row, get_rows, find_similar_item_get_best, dr
 from PIL import ImageDraw
 
 
+def run_it(network_name, eval_form_file_name, evaluation_form, eval_form_marked_file_name, cache_initial_matches, cache_parsed_rows, parallel=True):
+    net = network2.load(network_name)
+    if not eval_form_marked_file_name:
+        eval_form_marked_file_name = eval_form_file_name + '_' + os.path.basename(network_name) + u'_marked.jpg'
+    full_matches = None
+    if cache_initial_matches:
+        if os.path.exists(eval_form_file_name + '_' + os.path.basename(network_name) + u'_initial_matches'):
+            full_matches = pickle.load(
+                open(eval_form_file_name + '_' + os.path.basename(network_name) + u'_initial_matches', 'rb'))
+    if full_matches is None:
+        full_crops = net_crops_that_are_good(eval_form_file_name, net, 30, 30, printing=False)
+        full_matches = list(full_crops)
+        if cache_initial_matches:
+            pickle.dump(full_matches,
+                        open(eval_form_file_name + '_' + os.path.basename(network_name) + u'_initial_matches', 'wb'))
+    parsed_rows = None
+    parsed_rows_file_name = eval_form_file_name + '_' + os.path.basename(network_name) + u'_parsed_rows'
+    if cache_parsed_rows:
+        if os.path.exists(parsed_rows_file_name):
+            parsed_rows = pickle.load(
+                open(parsed_rows_file_name, 'rb'))
+    if parsed_rows is None:
+        s_fm = sorted(full_matches, key=lambda x: x.result[1][0])
+        better_fm = get_better(s_fm, net)
+        rows = get_rows(better_fm)
+        parsed_rows = list(rows)
+        if cache_parsed_rows:
+            pickle.dump(parsed_rows,
+                        open(parsed_rows_file_name, 'wb'))
+    eval_image = Image.open(evaluation_form)
+    eval_image = draw_hits(parsed_rows, eval_image)
+    eval_image.convert(u'RGB').save(eval_form_marked_file_name)
+    return eval_form_marked_file_name, parsed_rows, parsed_rows_file_name
+
+
+def get_input_vars(args):
+    network_name = args.network_name
+    eval_form_file_name = args.evaluation_form
+    evaluation_form = args.evaluation_form
+    eval_form_marked_file_name = args.evaluation_form_marked
+    cache_initial_matches = int(args.cache_initial_matches if args.cache_initial_matches != '' else 1) \
+        if isinstance(args.cache_initial_matches, str) else int(args.cache_initial_matches)
+    cache_parsed_rows = int(args.cache_parsed_rows if args.cache_parsed_rows != '' else 1) \
+        if isinstance(args.cache_parsed_rows, str) else int(args.cache_parsed_rows)
+    return network_name, eval_form_file_name, evaluation_form, eval_form_marked_file_name, cache_initial_matches, cache_parsed_rows
+
+
 if __name__ == '__main__':
     a_p = argparse.ArgumentParser()
     a_p.add_argument(u'network_name', type=str)
@@ -21,35 +68,5 @@ if __name__ == '__main__':
     a_p.add_argument(u'--cache_parsed_rows', type=int, default=1)
 
     args = a_p.parse_args()
-    net = network2.load(args.network_name)
-    eval_form_file_name = args.evaluation_form
-    eval_form_marked_file_name = args.evaluation_form_marked
-    if not eval_form_marked_file_name:
-        eval_form_marked_file_name = eval_form_file_name + '_' + os.path.basename(args.network_name) + u'_marked.jpg'
-    cache_initial_matches = int(args.cache_initial_matches if args.cache_initial_matches != '' else 1) \
-        if isinstance(args.cache_initial_matches, str) else int(args.cache_initial_matches)
-    cache_parsed_rows = int(args.cache_parsed_rows if args.cache_parsed_rows != '' else 1) \
-        if isinstance(args.cache_parsed_rows, str) else int(args.cache_parsed_rows)
-    full_matches = None
-    if cache_initial_matches:
-        if os.path.exists(eval_form_file_name + '_' + os.path.basename(args.network_name) + u'_initial_matches'):
-            full_matches = pickle.load(open(eval_form_file_name + '_' + os.path.basename(args.network_name) + u'_initial_matches', 'rb'))
-    if full_matches is None:
-        full_crops = net_crops_that_are_good(eval_form_file_name, net, 30, 30, printing=False)
-        full_matches = list(full_crops)
-        if cache_initial_matches:
-            pickle.dump(full_matches, open(eval_form_file_name + '_' + os.path.basename(args.network_name) + u'_initial_matches', 'wb'))
-    parsed_rows = None
-    if cache_parsed_rows:
-        if os.path.exists(eval_form_file_name + '_' + os.path.basename(args.network_name) + u'_parsed_rows'):
-            parsed_rows = pickle.load(open(eval_form_file_name + '_' + os.path.basename(args.network_name) + u'_parsed_rows', 'rb'))
-    if parsed_rows is None:
-        s_fm = sorted(full_matches, key=lambda x: x.result[1][0])
-        better_fm = get_better(s_fm, net)
-        rows = get_rows(better_fm)
-        parsed_rows = list(rows)
-        if cache_parsed_rows:
-            pickle.dump(parsed_rows, open(eval_form_file_name + '_' + os.path.basename(args.network_name) + u'_parsed_rows', 'wb'))
-    eval_image = Image.open(args.evaluation_form)
-    eval_image = draw_hits(parsed_rows, eval_image)
-    eval_image.convert(u'RGB').save(eval_form_marked_file_name)
+
+    run_it(*get_input_vars(args))
